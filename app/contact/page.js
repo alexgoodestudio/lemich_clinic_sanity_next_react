b7bff83dd59dfc67fe745c6d982cbb8254f5f27d
Alex Goode <alexgoodestudio@gmail.com>
2025-10-29 23:38:07 -0400

    checkers4

--- app/contact/page.js
+++ app/contact/page.js
@@ -74,7 +74,7 @@ function ContactForm() {
               Text: <strong>838255</strong>
             </p>
           </div>
-          
+          <a
             href="/contact"
             className="btn bg-slate-100 text-slate-900 border-0 px-4 py-2 rounded-0"
             style={{ 
